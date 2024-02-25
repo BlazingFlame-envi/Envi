f18b21cedfe104d7778e7624587ec6cae82d38c1
saif257 <saif.bouhamed@esprit.tn>
2024-02-25 15:10:02 +0100

    feat user with metier

---
+++ ENVIGestion/src/Controller/UserController.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Form\UserFormType;
+use App\Form\UsermodifyType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserController extends AbstractController
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    #[Route('/user', name: 'app_user')]
+    public function index_front(UserRepository $fournisseurRepository): Response
+    {
+               $authors=$fournisseurRepository->findAll(); 
+
+         return $this->render('user/account.html.twig', array(
+            'authors' => $authors,
+            
+        ));
+    }
+    #[Route('/user_display_front/{id}', name: 'user_show_details')]
+    public function show_details_modify(UserRepository $repository, $id,EntityManagerInterface $entityManagerInterface , Request $request): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $repository->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+        $form = $this->createForm(UsermodifyType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManagerInterface->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('user_show_details',  );
+        }
+        return $this->render('/user/account.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/user/fedit/{id}', name: 'app_user_fedit')]
+    public function fedit(Request $request,$id ,EntityManagerInterface $entityManagerInterface , UserRepository $authorRepository)
+    {
+       $author = $authorRepository->find($id);
+       $form=$this->createForm(UsermodifyType::class,$author);
+       $form->handleRequest($request);
+       if($form->isSubmitted() && $form->isValid())
+       {
+       $entityManagerInterface->persist($author);
+       $entityManagerInterface->flush();
+       return $this->redirectToRoute('app_user', ['id' => $author->getId()]);
+       }
+       return $this->renderForm('user/modifier.html.twig',['form'=>$form]);
+       dump($author);
+       die();
+    }
+
+    #[Route('/user/fnew', name: 'app_user_fnew')]
+    public function fnew(Request $request , EntityManagerInterface $entityManagerInterface)
+    {
+       $author = new User();
+       
+       $form=$this->createForm(UserFormType::class,$author);
+       $form->handleRequest($request);
+       if($form->isSubmitted() && $form->isValid() ){
+        $author=$form->getData();
+        $password=$author->getPassword();
+        $hashedpassword= $this->hasher->hashPassword(
+            $author,
+            $password
+        );
+        $author->setMotdepasse($hashedpassword);
+       $entityManagerInterface->persist($author);
+       $entityManagerInterface->flush();
+       return $this->redirectToRoute('app_login');
+       }
+       return $this->render('user/signin.html.twig',['form'=> $form->createView(),]);
+      // dump($author);
+      // die();
+    }
+
+    
+    #[Route('/afficheruser', name: 'app_user1')]
+    public function index3(UserRepository $authorRepository): Response
+    {
+        $authorss=$authorRepository->findAll();
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'AuthorController',
+            'u' => $authorss,
+            
+        ]);
+    }
+    #[Route('/user/delete/{id}', name: 'app_user_back_delete')]
+    public function delete($id ,EntityManagerInterface $entityManagerInterface , UserRepository $authorRepository)
+    {
+       $author = $authorRepository->find($id);
+       $entityManagerInterface->remove($author);
+       $entityManagerInterface->flush();
+       return $this->redirectToRoute('app_user_back');
+       dd($author);
+       
+    }
+    #[Route('/user/back', name: 'app_user_back')]
+    public function index_back(UserRepository $fournisseurRepository): Response
+    {
+               $authors=$fournisseurRepository->findAll(); 
+
+         return $this->render('user/afficher_back.html.twig', array(
+            'authors' => $authors,
+            
+        ));
+    }
+    #[Route('/user/fnew/back', name: 'app_user_fnew_back')]
+    public function fnew_back(Request $request , EntityManagerInterface $entityManagerInterface)
+    {
+       $author = new User();
+       
+       $form=$this->createForm(UserFormType::class,$author);
+       $form->handleRequest($request);
+       if($form->isSubmitted() && $form->isValid() ){
+        $author=$form->getData();
+        $password=$author->getPassword();
+        $hashedpassword= $this->hasher->hashPassword(
+            $author,
+            $password
+        );
+        $author->setMotdepasse($hashedpassword);
+       $entityManagerInterface->persist($author);
+       $entityManagerInterface->flush();
+       return $this->redirectToRoute('app_user_back');
+       }
+       return $this->render('user/ajouter_back.html.twig',['form'=> $form->createView(),]);
+      // dump($author);
+      // die();
+    }
+    #[Route('/{id}', name: 'user_active')]
+    public function user_active($id, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        if(!$id) {
+            throw $this->createNotFoundException('No ID found');
+        }
+        $user = $entityManager->getRepository(User::class)->find($id);
+        if($user != null) {
+            $user->setStatus("active");
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('app_user_back');
+    }
+    #[Route('/{id}/inactive', name: 'user_inactive')]
+public function user_inactive($id, EntityManagerInterface $entityManager): Response
+{
+    $user = $entityManager->getRepository(User::class)->find($id);
+
+    if (!$user) {
+        throw $this->createNotFoundException('User not found');
+    }
+
+    $user->setStatus("inactive");
+    $entityManager->flush();
+
+    return $this->redirectToRoute('app_user_back');
+}
+
+}
---
+++ ENVIGestion/src/Form/UsermodifyType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class UsermodifyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom',null, [
+                'label' => 'nom ',
+                'empty_data' => ''
+            ])
+            ->add('prenom',null, [
+                'label' => 'prenom ',
+                'empty_data' => ''
+            ])
+            ->add('mail',null, [
+                'label' => 'mail ',
+                'empty_data' => ''
+            ])
+            ->add('DN')
+            ->add('Update', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success btn-lg',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
